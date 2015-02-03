db6319e577ae32c6040559ecbab48fce4148e8d9
Marvin Oßwald <mail@marvinosswald.de>
2015-02-03 19:52:08 +0100

    removed dirs

--- tmp/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-var _interopRequire = function (obj) { return obj && obj.__esModule ? obj["default"] : obj; };
-
-var ai = _interopRequire(require("./modules/ai"));
-
-global.ai = require("./modules/ai");
